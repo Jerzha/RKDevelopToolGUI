7a0b2acdcd4348c49e11d4b2b932dcdfde3656dc
Zhang Hao <zhanghao@rock-chips.com>
2018-08-28 16:37:47 +0800

    add rkdeveloptool binary and setup.py for py2app

--- cmd.py
+++ cmd.py
@@ -18,47 +18,37 @@ def __get_command_process(cmd):
 
 
 def version():
-    return __do_command('rkdeveloptool -v')
+    return __do_command('./rkdeveloptool -v')
 
 
 def list_device():
-    return __do_command('rkdeveloptool ld')
+    return __do_command('./rkdeveloptool ld')
 
 
 def reset_device():
-    return __do_command('rkdeveloptool rd')
+    return __do_command('./rkdeveloptool rd')
 
 
 def upgrade_loader(file):
     print("Writing Loader " + file)
-    return __do_command('rkdeveloptool ul ' + file)
+    return __do_command('./rkdeveloptool ul ' + file)
 
 
 def write_parameter(file):
     print("Writing Parameter " + file)
-    return __do_command('rkdeveloptool prm ' + file)
+    return __do_command('./rkdeveloptool prm ' + file)
 
 
 def write_lba_bysec(begin_sec, file):
     print("Writing " + begin_sec + " " + file)
-    return __do_command('rkdeveloptool wl ' + begin_sec + ' ' + file)
+    return __do_command('./rkdeveloptool wl ' + begin_sec + ' ' + file)
 
 
 def write_lba_bysec_async(begin_sec, file):
     print("AWriting " + begin_sec + " " + file)
-    return __get_command_process('rkdeveloptool wl ' + begin_sec + ' ' + file)
+    return __get_command_process('./rkdeveloptool wl ' + begin_sec + ' ' + file)
 
 
 def write_lba_byname(name, file):
     print("Writing " + name + " " + file)
-    return __do_command('rkdeveloptool wlx ' + name + ' ' + file)
-
-
-if __name__ == '__main__':
-    ret = __get_command_process('curl www.baidu.com')
-    while ret.poll() is None:
-        line = ret.stdout.readline()
-        print('RD:' + str(line))
-
-    print('END: ' + str(ret))
-
+    return __do_command('./rkdeveloptool wlx ' + name + ' ' + file)
